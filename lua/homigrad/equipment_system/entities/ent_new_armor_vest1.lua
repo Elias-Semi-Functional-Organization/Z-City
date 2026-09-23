ebf1f7a4b28267710db2ccaa01a2d11bc1324795
sadsalat <74601305+sadsalat@users.noreply.github.com>
2026-09-24 00:07:47 +0300

    oops

--- lua/homigrad/equipment_system/entities/ent_new_armor_vest1.lua
+++ lua/homigrad/equipment_system/entities/ent_new_armor_vest1.lua
@@ -103,7 +103,7 @@ ENT.SlotOccupation = {                              -- Slots what armor occupate
     }
     --\\ FrontPlate
         ENT[FP] = {}
-        ENT[FP].Protection = ZC_ARMOR_PROTCLASS_III
+        ENT[FP].Protection = ZC_ARMOR_PROTCLASS_IV
             --\\ Protection classes
                 -- ZC_ARMOR_PROTCLASS_II = 4
                 -- ZC_ARMOR_PROTCLASS_IIIA = 8
@@ -132,7 +132,7 @@ ENT.SlotOccupation = {                              -- Slots what armor occupate
     
     --\\ BackPlate
         ENT[BP] = {}
-        ENT[BP].Protection = ZC_ARMOR_PROTCLASS_III
+        ENT[BP].Protection = ZC_ARMOR_PROTCLASS_IV
         ENT[BP].ProtectionDamageMul = 0.4                   -- protected damage mul
         ENT[BP].PenetratedDamageMul = 0.7                   -- penetrated damage mul
 
